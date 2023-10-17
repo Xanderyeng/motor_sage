0b9f359cd68955a85ebcc6150be347bd0015da63
Xanderyeng <50344667+Xanderyeng@users.noreply.github.com>
2023-10-17 23:55:58 +0300

    Skeleton of Case Study - Next JS

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,10 @@
 import './globals.css'
 import type { Metadata } from 'next'
+import "../public/css/style.css"
+import "../public/css/flexslider.css"
+import "../public/css/easy-responsive-tabs.css"
+import "../public/css/popuo-box.css"
+import "../public/css/bootstrap.css"
 import { Inter } from 'next/font/google'
 
 const inter = Inter({ subsets: ['latin'] })
@@ -16,7 +21,17 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>{children}
+      {/* <!-- smoothscroll --> */}
+	<script src="js/SmoothScroll.min.js"></script>
+	{/* <!-- //smoothscroll --> */}
+
+	{/* <!-- start-smooth-scrolling --> */}
+	<script src="js/move-top.js"></script>
+	<script src="js/easing.js"></script>
+  {/* <!-- for bootstrap working --> */}
+	<script src="js/bootstrap.js"></script>
+      </body>
     </html>
   )
 }
